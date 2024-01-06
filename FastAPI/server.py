bef8b5989e7357efee7fc00f587fd5e40547f041
Michal Lauer <michal.lauer.25@gmail.com>
2024-01-06 17:18:34 +0100

    Folder restructure for composer

--- FastAPI/server.py
+++ FastAPI/server.py
@@ -6,9 +6,9 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 app = FastAPI()
 
 # Load your pre-trained GPT-2 model and tokenizer
-model_path = "./trained_gpt2_model"
-tokenizer = GPT2Tokenizer.from_pretrained(model_path)
-model = GPT2LMHeadModel.from_pretrained(model_path)
+model_path = "./app/trained_gpt2_model"
+tokenizer = GPT2Tokenizer.from_pretrained(model_path, local_files_only=True)
+model = GPT2LMHeadModel.from_pretrained(model_path, local_files_only=True)
 
 # API call
 @app.post("/generate_text")
@@ -26,3 +26,9 @@ async def generate_text(request: dict):
                             do_sample=True)
     generated_text = tokenizer.batch_decode(output[:, input_ids.shape[1]:])[0]
     return {"generated_text": generated_text}
+
+# API call
+@app.get("/")
+async def read_root():
+    import os
+    print(os.listdir("./app/trained_gpt2_model"))
